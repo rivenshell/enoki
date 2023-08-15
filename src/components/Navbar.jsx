0dea363e3b2a76d9bfaac3d9d2baea92fb105315
Rivenshell Salomon <rivenshell111@gmail.com>
2023-08-15 15:46:58 -0400

    enoki logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,11 @@ function Navbar() {
       <nav class="bg-white border-gray-200 dark:bg-gray-900">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="https://flowbite.com/" class="flex items-center">
-            <img src="../enoki.png" class="h-8 mr-3" alt="Flowbite Logo" />
+            <img
+              src="src/assets/enoki1.svg"
+              class="h-8 mr-3"
+              alt="Flowbite Logo"
+            />
           </a>
           <button
             data-collapse-toggle="navbar-default"
@@ -45,7 +49,7 @@ function Navbar() {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="about"
                   class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   About
@@ -53,7 +57,7 @@ function Navbar() {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="services"
                   class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Services
@@ -61,7 +65,7 @@ function Navbar() {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="/pricing"
                   class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Pricing
