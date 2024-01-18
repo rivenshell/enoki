90b05345602f106da6a3ef39f510c349bf320e3c
rivenshell salomon <Rsalomon@stagewood.com>
2024-01-18 13:22:16 -0500

    project upgrade to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Footer from "./components/Footer"
 
 export default function MyPage() {
   return (
     <>
+      {/* site core elemenrs */}
       <Navbar />
       <Hero />
+      <Footer />
       <script>console.log("ok so i have an lets make it work idea")</script>
     </>
   )
